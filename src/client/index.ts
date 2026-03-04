1399cc8d8604a19a587285aa6b54420408cb6a02
Ren Koya <koya.ren.701@gmail.com>
2026-03-04 10:44:28 +0900

    add use agent

--- src/client/index.ts
+++ src/client/index.ts
@@ -47,6 +47,8 @@ export class NoteAPIClient {
         headers: {
           Cookie: this.cookies,
           "Content-Type": "application/json",
+          "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
         },
       });
 
